394c22ea2e31eeb572642fc5cd84a13ebd1bcfd5
Mihail Paley <undeadlol1@mail.ru>
2020-01-15 21:44:05 +0500

    (triggers_list) add appBar

--- lib/screens/triggers_list_screen.dart
+++ lib/screens/triggers_list_screen.dart
@@ -1,11 +1,15 @@
+import 'package:addictions_flutter/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
 class TriggesListScreen extends StatelessWidget {
+  static const routeName = '/create-addiction';
+
   const TriggesListScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Addictions')),
       body: Center(
         child: Text('This is triggers page'),
       ),
