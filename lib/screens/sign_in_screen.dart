1b052d5532e5b344d21385d14259628076100b79
Mihail Paley <undeadlol1@mail.ru>
2020-01-26 22:53:41 +0500

    (SignInScreen) add ActivityIndicator

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -3,14 +3,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
+  static const routeName = '/sign-in';
   SignInScreen({Key key}) : super(key: key);
 
-  static const routeName = '/sign-in';
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  bool _isLoading = false;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<FirebaseUser> _handleSignIn() async {
+    setState(() => _isLoading = true);
+
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
@@ -22,6 +30,8 @@ class SignInScreen extends StatelessWidget {
 
     final FirebaseUser user =
         (await _auth.signInWithCredential(credential)).user;
+
+    Navigator.pushNamed(context, '/');
     return user;
   }
 
@@ -30,13 +40,15 @@ class SignInScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Sign In')),
       body: Center(
-          child: Container(
-        height: 50,
-        child: SignInButton(Buttons.Google,
-            onPressed: () => _handleSignIn()
-                // .then((FirebaseUser user) => print(user))
-                .catchError((e) => print(e))),
-      )),
+          child: _isLoading
+              ? CircularProgressIndicator()
+              : Container(
+                  height: 50,
+                  child: SignInButton(Buttons.Google,
+                      onPressed: () => _handleSignIn()
+                          // .then((FirebaseUser user) => print(user))
+                          .catchError((e) => print(e))),
+                )),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print('button is pressed'),
         child: Icon(Icons.add),
