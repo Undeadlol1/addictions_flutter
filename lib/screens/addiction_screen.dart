8f01fc2fa58e1eea5d95840ec6c01f465fcd2785
Mihail Paley <undeadlol1@mail.ru>
2020-01-27 16:09:07 +0500

    (AddictionScreen) add speed dial

--- lib/screens/addiction_screen.dart
+++ lib/screens/addiction_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class AddictionScreen extends StatelessWidget {
   static const routeName = '/addiction_screen';
@@ -23,6 +24,44 @@ class AddictionScreen extends StatelessWidget {
           ListTile(title: Text('Tea')),
           Divider(),
           ListTile(leading: Icon(Icons.mood), title: Text('Alternatives')),
+        ],
+      ),
+      floatingActionButton: SpeedDial(
+        child: Icon(Icons.add),
+        // animatedIcon: AnimatedIcons.menu_close,
+        // animatedIconTheme: IconThemeData(size: 22.0),
+        // If true user is forced to close dial manually
+        // by tapping main button and overlay is not rendered.
+        closeManually: false,
+        curve: Curves.bounceIn,
+        // overlayColor: Colors.black,
+        // overlayOpacity: 0.5,
+        onOpen: () => print('OPENING DIAL'),
+        onClose: () => print('DIAL CLOSED'),
+        tooltip: 'Speed Dial',
+        heroTag: 'speed-dial-hero-tag',
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.mood),
+            backgroundColor: Colors.blue,
+            label: 'Alternative',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('FIRST CHILD'),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.mood_bad),
+            backgroundColor: Colors.deepPurpleAccent,
+            label: 'Consequence',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('SECOND CHILD'),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.warning),
+            backgroundColor: Colors.red,
+            label: 'Trigger',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('THIRD CHILD'),
+          ),
         ],
       ),
     );
